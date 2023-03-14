231ae03a34f5d9db055397404222e5f8a3d8a5ff
Jenseko <jenscode23@gmail.com>
2023-03-14 14:42:47 +0100

    all in one file

---
+++ assets/js/main.js
@@ -0,0 +1,128 @@
+// console.log("Tohuwabohu");
+
+// ==================================================
+
+// ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈ lvl_1-1: Arrays ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈
+
+const perSon = ["Jens","Potsdam","Vegan"];
+const friEnd = ["Andreas","Paul","Friedrich"];
+const favoriteFood = ["Sweet-Potato-Chili", "Pizza", "Peanut-Curry"];
+
+// console.log(perSon);
+// console.log(friEnd);
+// console.log(favoriteFood);
+
+
+// // ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈ lvl_1-2: Arrays Index ≈≈≈≈≈≈≈≈≈≈≈≈
+
+// console.log(perSon[0]);
+// console.log(perSon[1]);
+// console.log(perSon[2]);
+// console.log(friEnd[0]);
+// console.log(friEnd[1]);
+// console.log(friEnd[2]);
+// console.log(favoriteFood[0]);
+// console.log(favoriteFood[1]);
+// console.log(favoriteFood[2]);
+
+
+// // ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈ lvl_1-3: Arrays Length ≈≈≈≈≈≈≈≈≈≈≈
+
+// console.log(perSon.length);
+// console.log(friEnd.length);
+// console.log(favoriteFood.length);
+
+
+// // ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈ lvl_1-4: Arrays push() ≈≈≈≈≈≈≈≈≈≈≈
+
+// console.log(perSon);
+// console.log(perSon.length);
+// console.log(perSon.push("Gitarre"));
+// console.log(perSon);
+// console.log(perSon.length);
+
+// console.log(friEnd);
+// console.log(friEnd.length);
+// console.log(friEnd.push("Erich","Konstantin"));
+// console.log(friEnd);
+// console.log(friEnd.length);
+
+// console.log(favoriteFood);
+// console.log(favoriteFood.length);
+// console.log(favoriteFood.push("Sushi", "Burger"));
+// console.log(favoriteFood);
+// console.log(favoriteFood.length);
+
+
+// // ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈ lvl_1-5: Arrays pop() ≈≈≈≈≈≈≈≈≈≈≈
+
+// console.log(perSon);
+// let lastValue1 = perSon.pop();
+// console.log(lastValue1)
+// console.log(perSon);
+
+// console.log(friEnd);
+// let lastValue2 = friEnd.pop();
+// console.log(lastValue2);
+// console.log(friEnd);
+
+// console.log(favoriteFood);
+// let lastValue3 = favoriteFood.pop();
+// console.log(lastValue3);
+// console.log(favoriteFood);
+
+
+// ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈ lvl_1-6: Arrays shift() ≈≈≈≈≈≈≈≈≈≈
+
+// console.log(perSon);
+// let firstValue1 = perSon.shift();
+// console.log(firstValue1)
+// console.log(perSon);
+
+// console.log(friEnd);
+// let firstValue2 = friEnd.shift();
+// console.log(firstValue2);
+// console.log(friEnd);
+
+// console.log(favoriteFood);
+// let firstValue3 = favoriteFood.shift();
+// console.log(firstValue3);
+// console.log(favoriteFood);
+
+
+// ≈≈≈≈≈≈≈≈≈≈≈≈≈≈ lvl_1-7: Arrays unshift() ≈≈≈≈≈≈≈≈≈
+
+// console.log(perSon);
+// console.log(perSon.unshift("Hubert"));
+// console.log(perSon);
+
+// console.log(friEnd);
+// console.log(friEnd.unshift("Thomas","Alfred"));
+// console.log(friEnd);
+
+// console.log(favoriteFood);
+// console.log(favoriteFood.unshift("Spagetti Bolo"));
+// console.log(favoriteFood);
+
+
+// ≈≈≈≈≈≈≈≈≈≈≈≈≈≈ lvl_1-9: Arrays slice() ≈≈≈≈≈≈≈≈≈
+
+// let favoriteDestinations = ["Thailand", "Spanien", "Südafrika","Frankreich", "Schweden"];
+// console.log(favoriteDestinations);
+// let resulT = favoriteDestinations.slice(1,3);
+// console.log(resulT);
+
+// ≈≈≈≈≈≈≈≈≈≈≈≈≈≈ lvl_1-10: Arrays/String split() ≈≈≈≈≈≈≈≈≈
+
+// const text = "It's okay, Bender. I like cooking too. Oh, you're a dollar naughtier than most. My fellow Earthicans, as I have explained in my book 'Earth in the Balance', and the much more popular 'Harry Potter and the Balance of Earth', we need to defend our planet against pollution. Also dark wizards."
+
+// let vaLue1 = text.split(".");
+// console.log(vaLue1);
+
+
+// let vaLue2 = text.split(" ");
+// console.log(vaLue2);
+
+// let vaLue3 = text.split("");
+// console.log(vaLue3);
+
